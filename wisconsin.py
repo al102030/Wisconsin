62e2d5fcf3e7759eb32f085cd5e5034adefaad23
adarvishi <ali.darvishi.k@gmail.com>
2019-06-29 16:03:38 +0430

    Disable fillna and KNN Tuning

--- wisconsin.py
+++ wisconsin.py
@@ -39,8 +39,8 @@ dataset = dataset.replace({"Nuclei": dict1})
 # # Remove Rows With Missing Values
 # dataset.dropna(inplace=True)
 
-# fill missing values with mean column values
-dataset.fillna(dataset.mean(), inplace=True)
+# # fill missing values with mean column values
+# dataset.fillna(dataset.mean(), inplace=True)
 
 # Head
 set_option('display.max_columns', 500)
@@ -148,21 +148,21 @@ pyplot.boxplot(results)
 ax.set_xticklabels(names)
 pyplot.show()
 
-# Tune scaled KNN
-scaler = StandardScaler().fit(X_train)
-rescaledX = scaler.transform(X_train)
-neighbors = [1,3,5,7,9,11,13,15,17,19,21]
-param_grid = dict(n_neighbors=neighbors)
-model = KNeighborsClassifier()
-kfold = KFold(n_splits=num_folds, random_state=seed)
-grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
-grid_result = grid.fit(rescaledX, Y_train)
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+# # Tune scaled KNN
+# scaler = StandardScaler().fit(X_train)
+# rescaledX = scaler.transform(X_train)
+# neighbors = [1,3,5,7,9,11,13,15,17,19,21]
+# param_grid = dict(n_neighbors=neighbors)
+# model = KNeighborsClassifier()
+# kfold = KFold(n_splits=num_folds, random_state=seed)
+# grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+# grid_result = grid.fit(rescaledX, Y_train)
+# print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+# means = grid_result.cv_results_['mean_test_score']
+# stds = grid_result.cv_results_['std_test_score']
+# params = grid_result.cv_results_['params']
+# for mean, stdev, param in zip(means, stds, params):
+#     print("%f (%f) with: %r" % (mean, stdev, param))
 
 # ensembles
 ensembles = []
